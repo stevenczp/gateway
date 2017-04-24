e498130f7e1f173878a10b5a36c430ee9439f742
cc <stevenczp@gmail.com>
2017-04-24 22:32:44 +0800

    init

---
+++ src/main/java/com/steven/gateway/Startup.java
@@ -0,0 +1,10 @@
+package com.steven.gateway;
+
+/**
+ * Created by cc on 2017/4/24.
+ */
+public class Startup {
+    public static void main(String[] args) {
+        System.out.println("hell world");
+    }
+}
